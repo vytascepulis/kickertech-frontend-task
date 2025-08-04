2f6fc5c8b2f4e56af114ea78e9561ee304a01547
Vytautas <cepulis.vytas@gmail.com>
2025-08-04 14:20:36 +0300

    text sm on eurobasket comp

--- src/containers/EuroBasket/index.tsx
+++ src/containers/EuroBasket/index.tsx
@@ -26,7 +26,7 @@ const EuroBasket = () => {
   }
 
   return (
-    <div className='min-h-[300px] w-full max-w-[500px] rounded-lg bg-[#002e26] font-[montserrat] shadow-lg'>
+    <div className='min-h-[300px] w-full max-w-[500px] rounded-lg bg-[#002e26] font-[montserrat] text-sm shadow-lg'>
       <div className='flex justify-between p-4 text-2xl font-semibold text-white uppercase'>
         <div>
           <FontAwesomeIcon icon={faBasketball} className='mr-2' /> Eurobasket
