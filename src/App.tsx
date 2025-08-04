9f3b1dd65c2980c87e1701b5b6d548c1bf1f4450
Vytautas <cepulis.vytas@gmail.com>
2025-08-04 12:38:00 +0300

    updated main layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import EuroBasket from 'containers/EuroBasket';
 
 function App() {
   return (
-    <div className='mt-[150px] flex h-screen flex-wrap items-start justify-center gap-[30px]'>
-      <PremierLeague />
-      <EuroBasket />
+    <div className='flex justify-center'>
+      <div className='mt-[30px] grid gap-[30px] md:mt-[150px] md:grid-cols-2 lg:grid-cols-[repeat(3,minmax(250px,450px))]'>
+        <PremierLeague />
+        <EuroBasket />
+      </div>
     </div>
   );
 }
